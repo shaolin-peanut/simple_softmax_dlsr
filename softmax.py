da774fc18408372cc9e908ccb1393395937b66f8
shaolin-peanut <shaolinpeanut@gmail.com>
2024-06-17 18:40:03 +0200

    main function finally, and crossentropy loss function

--- softmax.py
+++ softmax.py
@@ -2,43 +2,57 @@ import pandas as pd
 import numpy as np
 import math
 
-data = pd.read_csv("datasets/dataset_train.csv").dropna()
+def cross_entropy_loss(true_labels, predictions):
+    return -np.sum(true_labels * np.log(predictions), axis=0)
 
-rowsc = data.shape[0]
-valrowsc = int(rowsc * 0.2)
-# * 0.2 gets 20% percent of rows count
+def softmax(x_t, y_t):
 
-classes = data[['Hogwarts House']]
-# one hot encoding
-y = pd.get_dummies(classes, columns = ["Hogwarts House"]).values
+    # normalize
+    x_t_min = x_t.min(axis=0)
+    x_t_max = x_t.max(axis=0)
+    x_t = (x_t - x_t_min) / (x_t_max - x_t_min)
 
-# traning set to initialize input features, removed all columns containing strings for simplicity, for now
-x_t = data.drop(
-    ['Hogwarts House', 
-    'Birthday', 
-    'Best Hand', 
-    'Index', 
-    'First Name', 
-    'Last Name'], axis=1).values[:-valrowsc]
-y_t = y[:-valrowsc]
+    featurec = x_t.shape[1]
+    classc = y_t.shape[1]
+    weights = np.random.randn(classc, featurec)
 
-x_t_min = x_t.min(axis=0)
-x_t_max = x_t.max(axis=0)
-x_t = (x_t - x_t_min) / (x_t_max - x_t_min)
+    netinput = np.matmul(x_t, weights.T)
+    # netinput_stable = netinput - np.max(netinput, axis=1, keepdims=True)
 
-# validation set (to run the cost function later)
-x_v = data.drop(['Index', 'First Name', 'Last Name'], axis=1).values[-valrowsc:] # first 20% of data
-# normalize too
-# x_v = (x_v - x_t_min) / (x_t_max - x_t_min)
+    zexp = np.exp(netinput)
+    expsum = np.sum(zexp, axis=1, keepdims=True)
+    softmax_probabilities = zexp / (expsum + 1e-4)
 
-featurec = x_t.shape[1]
-classc = y_t.shape[1]
-weights = np.random.randn(classc, featurec)
+    print(cross_entropy_loss(y_t, softmax_probabilities))
 
-netinput = np.matmul(x_t, weights.T)
-netinput_stable = netinput - np.max(netinput, axis=1, keepdims=True)
+def main():
 
-zexp = np.exp(netinput_stable)
-expsum = np.sum(zexp, axis=1, keepdims=True)
-softmax_probabilities = zexp / (expsum + 1e-4)
-print(softmax_probabilities)
+    data = pd.read_csv("datasets/dataset_train.csv").dropna()
+
+    rowsc = data.shape[0]
+    valrowsc = int(rowsc * 0.2)
+    # * 0.2 gets 20% percent of rows count
+
+    classes = data[['Hogwarts House']]
+    # one hot encoding
+    y = pd.get_dummies(classes, columns = ["Hogwarts House"])
+
+    # traning set to initialize input features, removed all columns containing strings for simplicity, for now
+    x_t = data.drop(
+        ['Hogwarts House', 
+        'Birthday', 
+        'Best Hand', 
+        'Index', 
+        'First Name', 
+        'Last Name'], axis=1).values[:-valrowsc]
+    y_t = y[:-valrowsc]
+
+    softmax(x_t, y_t)
+    # validation set (to run the cost function later)
+    x_v = data.drop(['Index', 'First Name', 'Last Name', 'Birthday', 'Best Hand'], axis=1).values[-valrowsc:] # first 20% of data
+    # normalize too
+    # x_v = (x_v - x_t_min) / (x_t_max - x_t_min)
+
+
+if __name__ == "__main__":
+    main()
